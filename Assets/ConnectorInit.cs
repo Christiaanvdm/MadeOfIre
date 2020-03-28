00b22fef78e5c5202f2ade3adb6cf631a172873b
Christiaan van der Merwe <Christiaanvdm@gmail.com>
2020-03-28 22:30:43 +0200

    Init

---
+++ Assets/ConnectorInit.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ConnectorInit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnRoom() {
+        var rooms = Resources.LoadAll<GameObject>("Rooms/South");
+        var room = rooms.First();
+        GameObject newRoom = Instantiate(room) as GameObject;
+        var entrance = transform.Find("Entrance");
+
+        newRoom.transform.position = entrance.position + ( newRoom.transform.position - newRoom.transform.Find("Entrance").position);
+    }
+}
---
+++ Assets/StartRoomManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Complete
+{
+	public class StartRoomManager : MonoBehaviour
+	{
+		public GameObject exit;
+		void Start() {
+			var hallways = Resources.LoadAll<GameObject>("Hallways/North");
+			var hallway = hallways.First();
+			GameObject newHallways = Instantiate(hallway) as GameObject;
+			newHallways.transform.SetParent(this.transform.parent);
+			newHallways.transform.localPosition = exit.transform.localPosition;
+			newHallways.transform.position = exit.transform.position ;
+			newHallways.GetComponent<ConnectorInit>().SpawnRoom();
+		}
+	}
+}
